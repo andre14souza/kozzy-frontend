6fe19a9fb56ab75408adcd8c0bedf39062a66af5
andre14souzza@gmail.com <160801890+andre14souza@users.noreply.github.com>
2025-12-16 15:47:41 -0300

    Fix: Finalizando configuracao de cookies para Render/Vercel

--- app-kozzy/src/app/app.config.ts
+++ app-kozzy/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; // Importe se for usar HttpClient
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { WithCredentialsInterceptor } from './with-credentials.interceptor'; // Caminho correto
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AuthService } from './auth.service'; // Importe o AuthService
 
@@ -10,8 +11,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), // Configura o roteamento da aplicação
     provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: WithCredentialsInterceptor,
+      multi: true
+    },
     provideAnimations(), // Fornece o HttpClient para requisições HTTP (se necessário)
-    AuthService // Fornece o AuthService para injeção de dependência em toda a aplicação
+    AuthService,
+     // Fornece o AuthService para injeção de dependência em toda a aplicação
   ]
 };
 
